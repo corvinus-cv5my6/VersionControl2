e6659b9a6e0c3b13d38e97622cac948d22475888
Botond Kovács <botond.kovacs@stud.uni-corvinus.hu>
2021-10-08 11:26:09 +0200

    Adatok irasa

--- ExcelExport/ExcelExport/Form1.Designer.cs
+++ ExcelExport/ExcelExport/Form1.Designer.cs
@@ -36,11 +36,11 @@ namespace ExcelExport
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(106, 85);
+            this.dataGridView1.Location = new System.Drawing.Point(21, 85);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(423, 284);
+            this.dataGridView1.Size = new System.Drawing.Size(776, 284);
             this.dataGridView1.TabIndex = 0;
             // 
             // Form1
--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -14,6 +14,7 @@ namespace ExcelExport
 {
     public partial class Form1 : Form
     {
+        private int _millon = (int)Math.Pow(10, 6);
         
 
         RealEstateEntities context = new RealEstateEntities();
@@ -50,7 +51,7 @@ namespace ExcelExport
                 xlSheet = xlWB.ActiveSheet;
 
                 // Tábla létrehozása
-               // CreateTable(); // Ennek megírása a következő feladatrészben következik
+               CreateTable(); // Ennek megírása a következő feladatrészben következik
 
                 // Control átadása a felhasználónak
                 xlApp.Visible = true;
@@ -74,7 +75,9 @@ namespace ExcelExport
 
         private void CreateTable()
         {
-            /*string[] headers = new string[] {
+            
+
+            string[] headers = new string[] {
              "Kód",
              "Eladó",
              "Oldal",
@@ -83,8 +86,65 @@ namespace ExcelExport
              "Szobák száma",
              "Alapterület (m2)",
              "Ár (mFt)",
-             "Négyzetméter ár (Ft/m2)"};*/
+             "Négyzetméter ár (Ft/m2)"
+            
+            };
 
+            for (int i = 0; i < headers.Length; i++)
+            {
+                   xlSheet.Cells[1, i + 1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            int floorColumn = 6;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator)
+                {
+                    values[counter, 4] = "van";
+                }
+                else
+                {
+                    values[counter, 4] = "nincs";
+                }
+                
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+               
+                values[counter, 8] = string.Format("={0}/{1}*{2}", "H" + (counter + 2).ToString(), GetCell(counter + 2, floorColumn + 1), _millon.ToString());
+                counter++;
+            }
+
+            
+
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }
