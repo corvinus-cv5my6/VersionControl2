4d9616d9b712fe925bed42df6bd7256cb49f8650
Botond Kovács <botond.kovacs@stud.uni-corvinus.hu>
2021-10-29 23:38:46 +0200

    projekt+lista

---
+++ week06/week06/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
+using week06.Entities;
+using week06.MnbServiceReference;
+
+namespace week06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+    }
+
+}
