5610e2008f94949e4f4e7c30862a3a22e86d6350
Botond Kovács <botond.kovacs@stud.uni-corvinus.hu>
2021-11-11 13:51:14 +0100

    futoszalag

--- Christmas/Christmas/Entities/BallFactory.cs
+++ Christmas/Christmas/Entities/BallFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace Christmas.Entities
 {
-    public class BallFactory
+    public class BallFactory 
     {
+    
         public Ball CreateNew()
         {
             return new Ball();
