47c1955c8f5e2d2e00c9fc6d8cf4f7356bed9cb9
Botond Kovács <botond.kovacs@stud.uni-corvinus.hu>
2021-11-26 10:29:23 +0100

    accountcontroller.test

---
+++ UnitTestExample/UnitTestExample2.test/AccountControllerTestFixture.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample2.test
+{
+    public class AccountControllerTestFixture        
+
+    {
+        [Test]
+
+        public void TestEmail(string Email, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+
+        }
+
+    }
+}
---
+++ UnitTestExample/UnitTestExample2.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample2.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample2.test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("15b85d6b-8df5-4a5d-8359-82fe8579756b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
