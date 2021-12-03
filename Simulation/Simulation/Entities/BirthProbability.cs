6d00ab8ba536681b7df4beed666198cbeef12c52
Botond Kovács <botond.kovacs@stud.uni-corvinus.hu>
2021-12-03 10:18:49 +0100

    methods

---
+++ Simulation/Simulation/Entities/BirthProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Entities
+{
+    public class BirthProbability
+    {
+        public int Age {get; set;}
+
+        public int nbrOfChildren { get; set; }
+
+        public double P { get; set; }
+    }
+}
