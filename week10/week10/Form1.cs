5e7609035470e463ea0c52e8887d2e91b2ff9712
Botond Kovács <botond.kovacs@stud.uni-corvinus.hu>
2021-12-10 15:33:14 +0100

    dll insert

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace week10
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            
         }
+        
+
     }
 }
