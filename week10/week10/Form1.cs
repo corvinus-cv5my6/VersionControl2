595450da1c528b64b2f51dbb55c7d79a41967d27
Botond Kovács <botond.kovacs@stud.uni-corvinus.hu>
2021-12-10 15:45:50 +0100

    button

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -78,5 +78,14 @@ namespace week10
             }
             gc.Start();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
+        }
     }
 }
