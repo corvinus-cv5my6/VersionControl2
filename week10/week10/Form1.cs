36f586a94849dfff6d447bd10de3777ef687069b
Botond Kovács <botond.kovacs@stud.uni-corvinus.hu>
2021-12-10 15:35:07 +0100

    gamecontroller peldany

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -14,11 +14,15 @@ namespace week10
     
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
         public Form1()
         {
             InitializeComponent();
 
-            
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
         }
         
 
